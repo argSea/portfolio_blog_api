ff8ed0b54cd49202743d20421785e3c4aaeff813
Justin <moondrop@argsea.com>
2022-12-06 17:19:23 +0000

    More changes

--- argHex/core/resume/domain.go
+++ argHex/core/resume/domain.go
@@ -1,13 +1,11 @@
 package core
 
-import extra "github.com/argSea/portfolio_blog_api/argHex/core/extra/"
-
 type Resume struct {
-	Id            string            //`json:"projectID" bson:"_id,omitempty"`
-	UserID        string            //`json:"userID" bson:"userID,omitempty"`
-	About         string            //`json:"about" bson:"about,omitempty"`
-	Experiences   extra.Experiences //`json:"experiences" bson:"experiences,omitempty"`
-	Education     *[]Education      //`json:"education" bson:"education,omitempty"`
-	ExtraCourses  Courses           //`json:"extraCourses" bson:"extraCourses,omitempty"`
-	SkillSections SkillSections     //`json:"skills" bson:"skills,omitempty"`
+	Id            string        //`json:"projectID" bson:"_id,omitempty"`
+	UserID        string        //`json:"userID" bson:"userID,omitempty"`
+	About         string        //`json:"about" bson:"about,omitempty"`
+	Experiences   Experiences   //`json:"experiences" bson:"experiences,omitempty"`
+	Education     *[]Education  //`json:"education" bson:"education,omitempty"`
+	ExtraCourses  Courses       //`json:"extraCourses" bson:"extraCourses,omitempty"`
+	SkillSections SkillSections //`json:"skills" bson:"skills,omitempty"`
 }
