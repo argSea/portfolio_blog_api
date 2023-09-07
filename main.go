405f218a0ff5cfb310974d21d6b8e693128a3358
Justin <moondrop@argsea.com>
2023-09-07 04:06:15 -0400

    Cookie setting maybe

--- main.go
+++ main.go
@@ -185,7 +185,7 @@ func main() {
 
 func baseMiddleWare(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Origin", "argsea.com")
 		w.Header().Add("Content-Type", "application/json")
 
 		fmt.Println(r.URL)
