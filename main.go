2f9c5c320c3df1865488c6acd0d3b50326104e34
Justin <barbatos@argsea.com>
2023-12-02 17:28:22 -0500

    Filtering

--- main.go
+++ main.go
@@ -217,6 +217,7 @@ func baseMiddleWare(next http.Handler) http.Handler {
 
 		// check if query string is present starting with filter and path is /1/project
 		if "" != r.URL.Query().Get("filter") && r.URL.Path == "/1/project" {
+			log.Println("filter found")
 			// get filter
 			filter := r.URL.Query().Get("filter")
 
