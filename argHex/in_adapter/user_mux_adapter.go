46c728af0e8d31f4ef8c6d304c0793117392150d
Justin <barbatos@argsea.com>
2023-12-02 20:49:26 -0500

    Filtering

--- argHex/in_adapter/user_mux_adapter.go
+++ argHex/in_adapter/user_mux_adapter.go
@@ -664,10 +664,9 @@ func (u userMuxAdapter) GetProjects(w http.ResponseWriter, r *http.Request) {
 
 	total := len(response.Projects)
 
-	w.WriteHeader(http.StatusOK)
 	w.Header().Add("Content-Range", "users "+strconv.FormatInt(offset, 10)+"-"+strconv.FormatInt(offset+limit, 10)+"/"+strconv.FormatInt(int64(total), 10))
 	w.Header().Add("range", "users "+strconv.FormatInt(offset, 10)+"-"+strconv.FormatInt(offset+limit, 10)+"/"+strconv.FormatInt(int64(total), 10))
-
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(response)
 }
 
